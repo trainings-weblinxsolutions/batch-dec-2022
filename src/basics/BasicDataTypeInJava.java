27052ae6200b3d19bfd3790e88bdafddcdaf28d8
trainings-weblinxsolutions <info@weblinxsolutions.com>
2022-12-09 15:32:41 +0530

    Basic Java Programs

---
+++ src/basics/BasicDataTypeInJava.java
@@ -0,0 +1,18 @@
+public class BasicDataTypeInJava {
+
+  
+
+   public static void main(String []args) {
+      
+      byte a = 100;
+      System.out.println(a);
+      
+      int i = 40;
+      int j = 100;
+      System.out.println(i+j);
+      
+      
+      
+      
+   }
+}
