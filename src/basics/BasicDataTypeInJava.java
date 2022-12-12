5caea122030299491112e599b58b77c0985ef898
trainings-weblinxsolutions <info@weblinxsolutions.com>
2022-12-12 23:14:36 +0530

    Data Types and Scope of Variable

--- src/basics/BasicDataTypeInJava.java
+++ src/basics/BasicDataTypeInJava.java
@@ -1,18 +1,51 @@
 public class BasicDataTypeInJava {
 
-  
+
 
    public static void main(String []args) {
-      
-      byte a = 100;
-      System.out.println(a);
-      
-      int i = 40;
-      int j = 100;
-      System.out.println(i+j);
-      
-      
-      
-      
-   }
-}
+
+		byte a = 100; System.out.println(a);
+
+      int i = 40; int j = 100; System.out.println(i+j);
+
+
+
+      long n = 5000000l;
+
+       System.out.println(n);
+
+
+       float f = 12.3f; System.out.println(f);
+
+
+       double d = 54.89; System.out.println(d);
+
+      boolean b = true; System.out.println(b);
+
+       char cio = '%'; System.out.println(cio);
+
+
+       int myVValue = 100; int MyLabel = 10;
+
+
+       int age2022 = 19;
+
+       int $range = 100;
+
+       int my_value_balance=20000;
+       
+       
+       
+       int class_1 = 10;
+
+
+/*
+    int num1, num2, num3;
+
+    num1 = 10; num2 = 20; num3=30;
+    
+    System.out.println(num1+num2+num3); // this is used to add 3 numbers
+
+*/
+
+   } }
---
+++ src/basics/ScopeOfVariables.java
@@ -0,0 +1,20 @@
+public class ScopeOfVariables {
+   public static void main(String []args) {
+   int x =100;
+   
+   if(x > 10){
+   	int y=20;
+   	System.out.println(x+y);
+   
+   }
+   
+   	System.out.println(x+y);
+   
+   
+   
+   
+   
+   }
+
+
+}
