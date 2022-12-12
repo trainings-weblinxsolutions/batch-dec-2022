fcb97c04609e28cfd8b1107ed93057b63874b3ec
trainings-weblinxsolutions <info@weblinxsolutions.com>
2022-12-12 23:14:54 +0530

    Arithmetic Operators in Java

---
+++ src/basics/ArithmeticOperators.java
@@ -0,0 +1,89 @@
+public class ArithmeticOperators {
+   public static void main(String []args) {
+   
+   
+   int a = 10;
+   int b = 2;
+   
+   
+   /*
+   
+   System.out.println(a+b);
+   System.out.println(a-b);
+   System.out.println(a*b);
+   System.out.println(a/b);
+   
+   
+  a++;
+  b--;
+  
+    System.out.println(10%4); // when we want the remainder of 2 numbers, use % (modulo) operator.
+    
+   
+    // increment and decrement operator in Java
+  
+   // System.out.println(a);
+    
+    
+   
+    //System.out.println(b);
+    
+    
+  
+    int c = a++;
+    System.out.println(c); //
+    
+    System.out.println(a);
+    
+    
+    
+    int d = b--;
+    
+     System.out.println(d); //
+    
+    System.out.println(b);
+    
+ */ 
+    
+    ++a;
+    
+    System.out.println(a);
+    
+    int c = ++a;
+    System.out.println(c);
+    System.out.println(a);
+    
+   
+   
+    --b; // b=1;
+    System.out.println(b);
+    int d = --b;
+    
+     System.out.println(d);
+     System.out.println(b);
+    
+    
+    int number = 10;
+    //number = number + 5;
+    
+    number += 5;
+    
+    
+    System.out.println(number);
+    
+    
+        number -= 2;
+        System.out.println(number);
+
+    number *= 2;
+    System.out.println(number);
+    
+       number /= 13;
+        System.out.println(number);
+
+
+   
+   
+   
+   }
+}
