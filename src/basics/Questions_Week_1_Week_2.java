459d62b8e3447fdec8ac8112895d9a39b339f280
trainings-weblinxsolutions <info@weblinxsolutions.com>
2022-12-19 09:43:00 +0530

    QnA - Week 1,2

---
+++ src/basics/Questions_Week_1_Week_2.java
@@ -0,0 +1,65 @@
+public class Questions_Week_1_Week_2{
+
+
+public static void main(String []args) {
+
+
+// Question 2 - 10
+// Question 3 - Compile error
+// Question 4 - 10,20,11,21
+// Question 5 - 17,17
+// Question 6 - 0,30
+// Question 7 - 5,80
+// Question 8 - 9
+// Question 9 - 28 
+// Question 10 - 51 
+// Question 11
+
+
+
+
+
+
+int number = 10;
+if(number % 2 == 0){
+System.out.println("Even");
+}
+else{
+System.out.println("Odd");}
+
+
+
+
+
+// Question 12
+int marks = 62;
+
+switch(marks) {
+
+case 90:
+	System.out.println("Grade A");
+	break;
+case 80:
+	System.out.println("Grade B");
+	break;
+case 70:
+	System.out.println("Grade C");
+	break;
+case 60:
+	System.out.println("Grade D");
+	break;
+case 50:
+	System.out.println("Grade E");
+	break;
+default:
+	System.out.println("Invalid grade");
+	break;					
+
+}
+
+
+
+
+
+}
+}
