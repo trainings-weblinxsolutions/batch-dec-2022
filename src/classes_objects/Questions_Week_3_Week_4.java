9be0d1b9f4042290a7f3dbe2a8b59ec1fc05e8cc
trainings-weblinxsolutions <info@weblinxsolutions.com>
2023-01-04 23:19:13 +0530

    Ques Week 3,4

---
+++ src/classes_objects/Questions_Week_3_Week_4.java
@@ -0,0 +1,33 @@
+public class Questions_Week_3_Week_4{
+
+
+public static void main(String []args) {
+
+
+int [] arr1= {1, 2, 3};
+int [] arr2 = {1, 2, 3};
+if(arr1[0] == arr2[0])
+System.out.println("Same");
+else
+System.out.println("Not same");
+
+
+
+
+int intArray1[][]; // A
+int[][] intArray2; // B
+int[] intArray3[]; // C
+
+    
+T manish = new T();
+      System.out.println(manish.t);
+
+
+
+
+}
+}
+
+class T {
+  int t = 20;
+}
